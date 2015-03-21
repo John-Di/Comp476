ebb06ec9d7a61a79865418a2512981d329270ceb
adjnor <kalas001@hotmail.fr>
2015-03-21 12:19:48 -0400

    AIMovement fix for pathfinding

--- Assets/Code/AI/AIMovement.cs
+++ Assets/Code/AI/AIMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class AIMovement : MonoBehaviour
 {
-	private float timeStuck = 0.0f;	
+	//private float timeStuck = 0.0f;	
 	public float wanderDistance = 0.02f, wanderRadius = 0.005f, updateRange= 0.015f;
 	public float MaxVelocity = 0.1f, MaxAcceleration = 3f;
 	public Vector3 target;
@@ -12,15 +12,12 @@ public class AIMovement : MonoBehaviour
 
 	public List<Vector3> path;
 	bool halt;
-	public bool wander;
-	
+
 	void Start()
 	{
 		halt = false;
 		velocity = Vector3.zero;
 		path = new List<Vector3>();
-		UpdatePath(new Vector3(Random.Range(0f,50f), 0f, Random.Range(0f,50f)));
-		target = transform.position;
 	}
 
 	void Update()
@@ -34,7 +31,7 @@ public class AIMovement : MonoBehaviour
 		{
 			UpdatePosition(Time.deltaTime);
 		}
-	}	
+	}
 	
 	private void ResetVelocities()
 	{
@@ -43,21 +40,21 @@ public class AIMovement : MonoBehaviour
 	
 	private void UpdateTarget()
 	{
-		if(path.Count > 0 && transform.position == target)
+		if(path.Count > 0 && Vector3.Distance(transform.position, target) <= 0.5f)
 		{
 			target = path[0];
 			path.Remove(target);
-		}
-		else if(path.Count == 0 && wander)
+		}/* Allow poor wandering
+		else if(path.Count == 0)
 		{
 			path.Add(new Vector3(Random.Range(0f,50f), 0f, Random.Range(0f,50f)));
-		}
+		}*/
 		
 	}
 	
 	private void UpdateVelocities(float deltaTime)
 	{	
-		halt = (path.Count == 0 && transform.position == target);
+		halt = (path.Count == 0 && Vector3.Distance(transform.position, target) <= 0.1f);
 		
 		if(!halt)
 		{
@@ -79,16 +76,16 @@ public class AIMovement : MonoBehaviour
 	
 	private void UpdatePosition(float deltaTime)
 	{
-		Vector2 posDiff = new Vector2(target.x - transform.position.x, target.z - transform.position.z);
+		//Vector2 posDiff = new Vector2(target.x - transform.position.x, target.z - transform.position.z);
 		
-		if(target == null || posDiff.magnitude > 0.5f)
+		if(target == null || Vector3.Distance(transform.position, target) > 0.1f)
 		{
 			transform.position += velocity * deltaTime;
 		}
-		else
-		{
-			transform.position = target;
-		}
+//		else
+//		{
+//			transform.position = target;
+//		}
 	}	
 	
 	private void UpdateRotation(float deltaTime)
@@ -101,7 +98,7 @@ public class AIMovement : MonoBehaviour
 	public void UpdatePath(List<Vector3> p)
 	{
 		path = new List<Vector3>();
-
+		target = p[0];
 		AddToPath(p);
 
 	}
@@ -109,7 +106,7 @@ public class AIMovement : MonoBehaviour
 	public void UpdatePath(Vector3 p)
 	{
 		path = new List<Vector3>();
-		
+		target = p;
 		AddToPath(p);
 		
 	}
@@ -124,7 +121,7 @@ public class AIMovement : MonoBehaviour
 
 	public void AddToPath(Vector3 p)
 	{
-		path.Add(p);	
+		path.Add(p);
 	}
 
 	private void Seek()
