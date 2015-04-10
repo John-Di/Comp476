5eca2e1791068025825159266cd4f00d160044b7
kc-wan <martinwan1992@hotmail.com>
2015-04-10 17:15:33 -0400

    Made Mr.Gerald follow player and shoots the player when player is within range. Fireball kills player. Touching Mr.Gerald kills player. Fireball gets destroyed on contact with the environment or after 5 seconds.

--- Assets/Code/AI/Enemy Abilities/ProjectileScript.cs
+++ Assets/Code/AI/Enemy Abilities/ProjectileScript.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ProjectileScript : MonoBehaviour {
+public class ProjectileScript : MonoBehaviour
+{
 	float bulletSpeed = 2f;
-	float bulletLifeTime = 10f;
+	float bulletLifeTime = 5f;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		rigidbody.AddForce (transform.up * bulletSpeed);
 		Destroy (gameObject, bulletLifeTime);
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag ("Player") || other.CompareTag ("Wall"))
+		{
+			Destroy (gameObject);
+		}
 	}
 }
