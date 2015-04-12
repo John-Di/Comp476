989cb4088059d628a07d52ba910eeecf528b780b
leekelly12 <lee.kelly12@gmail.com>
2015-04-12 16:37:48 -0400

    Bug Finite State Machine (Almost done need stuff from John), Edit to room notifier to tell bugs where player is, Edit to waypoint graph for wandering node for bug. More tags, Bug Spawner to changed to the Trap Scream when john finishes....

---
+++ Assets/BugSpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BugSpawner : MonoBehaviour {
+	public int maxspawn = 4;
+	public GameObject bug;
+	GameObject b;
+	public int bugs;
+	public List<BugController> bc;
+	int bugnumber = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void spawnBug(){
+		if(bugs < maxspawn){
+			b = (GameObject)Instantiate (bug, GameObject.FindGameObjectWithTag("spawnpoint").transform.position, Quaternion.identity);
+			RoomNotifier.bc.Add(b.GetComponent<BugController>());
+			BugController.BugList.Add(b);
+			//Debug.Log("Bug Number "  + bugs);
+			if(bugs < maxspawn/2){
+				b.GetComponent<BugController>().groupNumber = 1;
+				BugController.BugGroup1.Add(b);
+			}else{
+				b.GetComponent<BugController>().groupNumber = 2;
+				BugController.BugGroup2.Add(b);
+			}
+		}
+		bugs++;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Player")){
+			spawnBug();
+
+			if(bugs == maxspawn){
+			//	Debug.Log("Notify done spawning");
+				bc = GameObject.FindGameObjectsWithTag ("BugController").Select(g => {
+					return g.GetComponent<BugController>();
+				}).ToList();
+
+				bc.ForEach(b => {b.NotifyBugSpawn();});
+			}
+		}
+	}
+}
