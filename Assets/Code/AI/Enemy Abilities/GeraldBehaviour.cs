1797d61cff0c0e664a9db2b8503ef2bb35234821
kc-wan <martinwan1992@hotmail.com>
2015-04-11 23:54:34 -0400

    Waypoints work. Gerald33 and Gerald39 somehow instantly activates CollisionAvoidance and deactivates AIMovement thus making Gerald walk through walls. Will try and find fix.

---
+++ Assets/Code/AI/Enemy Abilities/GeraldBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeraldBehaviour : MonoBehaviour
+{
+	GameObject[] geraldWaypoints;
+	PathfindingAgent pa;
+	int randomTarget;
+	Vector3 geraldZinWaypointSpace;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		pa = GetComponent<PathfindingAgent>();
+	}
+
+	void Start()
+	{
+		geraldWaypoints = GameObject.FindGameObjectsWithTag ("GeraldWaypoint");
+		NewWaypoint ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		CheckLocation ();
+	}
+
+	void NewWaypoint()
+	{
+		randomTarget = Random.Range (0, geraldWaypoints.Length);
+		pa.target = geraldWaypoints[randomTarget].transform;
+	}
+
+	void CheckLocation()
+	{
+		if(Vector3.Distance (pa.target.position, transform.position) <= 0.1f)
+		{
+			Debug.Log ("Reached target");
+			NewWaypoint ();
+		}
+	}
+}
