67ee0027df93d45cf03cf89499b099b85745353e
leekelly12 <lee.kelly12@gmail.com>
2015-04-13 21:00:50 -0400

    Added Win Scene, Music, edited the navMesh and the Map

---
+++ Assets/Code/WinGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			AutoFade.LoadLevel ("Win Screen", 2, 1, Color.white);
+		}
+	}
+}
